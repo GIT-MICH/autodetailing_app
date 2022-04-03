3885318bcf045d71f1984f3e4cef0a93c1457325
Zlakowski <zlakos@o2.pl>
2022-04-03 21:52:09 +0200

    tests

--- autodetailing_app/conftest.py
+++ autodetailing_app/conftest.py
@@ -1,1 +1,56 @@
 import pytest
+from django.contrib.auth.models import User
+
+from autodetailing_app.models import Service, Category, Worker, Order, Cart, Opinion, About
+
+
+@pytest.fixture
+def about():
+    return About.objects.create(description='This is some text to check')
+
+
+@pytest.fixture
+def category():
+    return Category.objects.create(name='example', description='Text to check')
+
+
+@pytest.fixture
+def category_id_2():
+    return Category.objects.create(pk=2)
+
+
+@pytest.fixture
+def user():
+    return User.objects.create_user(username='carpro', password='showcar')
+
+
+@pytest.fixture
+def some_id():
+    for i in range(1, 3):
+        return str(i)
+
+
+@pytest.fixture
+def service(some_id):
+    return Service.objects.create(pk=some_id)
+
+
+
+
+
+# @pytest.fixture
+# def author():
+#     return Author.objects.create(first_name='slawek', last_name='bo')
+#
+#
+# @pytest.fixture
+# def books(author):
+#     lst = []
+#     for i in range(10):
+#         x = Book.objects.create(
+#             title=i,
+#             year=i,
+#             author=author
+#         )
+#         lst.append(x)
+#     return lst
--- autodetailing_app/tests.py
+++ autodetailing_app/tests.py
@@ -1,3 +1,158 @@
 from django.test import TestCase
+from django.test import Client
+import pytest
 
-# Create your tests here.
+from django.urls import reverse
+
+from autodetailing_app.forms import AddServiceForm
+from autodetailing_app.models import Service
+
+
+@pytest.mark.django_db
+def test_main(about):
+    client = Client()
+    url = reverse('main')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_category(category):
+    client = Client()
+    url = reverse('categories')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_outside(category):
+    client = Client()
+    url = reverse('outside')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_inside(category_id_2):
+    client = Client()
+    url = reverse('inside')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_to_card_not_login(some_id):
+    client = Client()
+    url = reverse('add-service-to-card', args=some_id)
+    response = client.get(url)
+    assert response.status_code == 302
+    url = reverse('login')
+    assert response.url.startswith(url)
+
+
+@pytest.mark.django_db
+def test_add_to_card_login(user, some_id, service):
+    client = Client()
+    client.force_login(user)
+    url = reverse('add-service-to-card', args=some_id)
+    response = client.get(url)
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_add_service_view_get():
+    client = Client()
+    url = reverse('service-add')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert isinstance(response.context['form'], AddServiceForm)
+
+
+@pytest.mark.django_db
+def test_add_service_view_post():
+    client = Client()
+    url = reverse('service-add')
+    date = {
+        'name': 'example',
+        'description': 'some text',
+        'duration': 1,
+        'price': 1,
+    }
+    response = client.post(url, date)
+    assert response.status_code == 302
+    # new_url = reverse('services')
+    # assert response.url.startswith(new_url)
+    # Service.objects.get(**date)
+
+# @pytest.mark.django_db
+# def test_add_publisher_get():
+#     client = Client()
+#     url = reverse('add_publisher_model')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#     assert isinstance(response.context['form'], AddPublisherModelForm)
+#
+# @pytest.mark.django_db
+# def test_add_publisher_post():
+#     client = Client()
+#     url = reverse('add_publisher_model')
+#     date = {
+#         'name':'ala',
+#         'address' : 'poznanska',
+#         "street" : "poznanska",
+#         "city" : "bździna dolna",
+#         'description':'najlepsze wyd na świecie'
+#     }
+#     response = client.post(url, date)
+#     assert response.status_code == 302
+#     new_url = reverse('list_author')
+#     assert response.url.startswith(new_url)
+#     Publisher.objects.get(**date)
+
+
+# @pytest.mark.django_db
+# def test_booklist_login(user):
+#     client = Client()
+#     client.force_login(user)
+#     url = reverse('list_books')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#     assert response.context['object_list'].count() == 0
+
+# @pytest.mark.django_db
+# def test_booklist_not_login():
+#     client = Client()
+#     url = reverse('list_books')
+#     response = client.get(url)
+#     assert response.status_code == 302
+#     url = reverse('login')
+#     assert response.url.startswith(url)
+#
+
+#
+#
+# @pytest.mark.django_db
+# def test_booklist_login(user, books):
+#     client = Client()
+#     client.force_login(user)
+#     url = reverse('list_books')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#     assert response.context['object_list'].count() == len(books)
+#     for book in books:
+#         assert book in response.context['object_list']
+#
+#
+# @pytest.mark.django_db
+# def test_book_update_view(books):
+#     book = books[0]
+#     client = Client()
+#     url = reverse('update_book', args=(book.id,))
+#     data = {
+#         'title':'?',
+#         'year':123,
+#         'author': book.author.id#### TO JEST WAŻNE
+#     }
+#     response = client.post(url, data)
+#
+#     Book.objects.get(title="?")
