e0c8dac855aeaba82d7de2a0c84b5d8080dfa118
Michał Złakowski <90477632+GIT-MICH@users.noreply.github.com>
2022-04-29 23:08:15 +0200

    Update settings.py

--- PROJEKT_KONCOWY/settings.py
+++ PROJEKT_KONCOWY/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'autodetailing_db',
         'USER': 'postgres',
-        'PASSWORD': 'coderslab',
+        'PASSWORD': '',
     }
 }
 
